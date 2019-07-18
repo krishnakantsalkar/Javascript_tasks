192e71f1283bb8afde80f8ff264f24691d54dffe
krishnakantsalkar <krishnakantsalkar@gmail.com>
2019-07-18 23:12:32 +0530

    #krishnakant_salkar Task 1 initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+let numMonths= 3;
+let message="null"
+
+switch(numMonths){
+
+    case 1:
+        message="January"
+        break;
+
+    case 2:
+        message="February"
+        break;
+
+    case 3:
+        message="March"
+        break;
+
+    case 4:
+        message="April"
+        break;
+
+    case 5:
+        message="May"
+        break;
+        
+    case 6:
+        message="June"
+        break;
+        
+    case 7:
+        message="July"
+        break;
+
+    case 8:
+        message="August"
+        break;
+
+    case 9:
+        message="September"
+        break;
+
+    case 10:
+        message="October"
+        break;
+
+    case 11:
+        message="November"
+        break;
+
+    case 12:
+        message="December"
+        break;
+    
+    default:
+        message="should be between 1-12"
+        break;
+ 
+}
+
+console.log(message);
