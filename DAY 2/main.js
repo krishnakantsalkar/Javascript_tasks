73498250ac88df32d52460b28b799b07fa906ef2
krishnakantsalkar <krishnakantsalkar@gmail.com>
2019-07-20 17:04:35 +0530

    javascript task: fix fomartting of code and remove unnecessary lines and spaces

--- DAY 2/main.js
+++ DAY 2/main.js
@@ -1,20 +1,20 @@
     var num1 = [1,2,",3",",4",",5",",6",",7",",8",",9",",10"];
     var numODD = "";
     var numEVEN = "";
-    var comma = " ";
+    
     for(var i =0; i <=num1.length -1; i++){
-        if(i %2 === 0 ){
+      if(i %2 === 0 ){
         numODD += num1[i] + "";
         
-        }
-        else{ (i%3 === 0)
-        numEVEN +=  num1[i] +"" ;
-        }
-        
-        
       }
-      console.log(numODD);
-      console.log(numEVEN);
+      else{ (i%3 === 0)
+        numEVEN += num1[i] + "" ;
+      }
+        
+        
+    }
+    console.log(numODD);
+    console.log(numEVEN);
 
     
 
