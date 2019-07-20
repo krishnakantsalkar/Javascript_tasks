e6ba0a1311ba92918e129ae086698cf9dda5f5c7
krishnakantsalkar <krishnakantsalkar@gmail.com>
2019-07-20 15:21:52 +0530

    Javascript task: align console.log output in one line for even and odd from array

---
+++ DAY 2/main.js
@@ -0,0 +1,30 @@
+    var num1 = [1,2,3,4,5,6,7,8,9,10];
+    var numODD = "";
+    var numEVEN = "";
+    for(var i =0; i <= num1.length -1; i++){
+      if(i %2 === 0 ){
+        numODD += num1[i] + ", ";
+        
+        }
+        else{ (i%3 === 0)
+            numEVEN += num1[i] + ", ";
+        }
+        
+      }
+      console.log(numODD);
+      console.log(numEVEN);
+
+    
+
+
+// Used Method Reference from stackoverflow below
+//       var lst = [1,2,3,4,5];
+//       var output = "";
+    
+//     for(var i =0; i <= lst.length; i++){
+//         if(i %2 === 0 ){
+//         output += lst[i] + " ";
+//     }
+// }
+//     console.log(output);
+    
